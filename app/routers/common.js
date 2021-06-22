15fa3f234f3dfdb34c6d09702ea0ba5d21d45ca6
RitikaAgarwal1 <Ritika.Agarwal1@gds.ey.com>
2021-06-22 21:52:38 +0530

    added email template

--- app/routers/common.js
+++ app/routers/common.js
@@ -1,7 +1,27 @@
 const express = require('express');
 const router = new express.Router();
-const {filterFromData} = require('../library/dbQuery');
+const { filterFromData } = require('../library/dbQuery');
+const fs = require('fs');
 const { exeQuery } = require('../library/db');
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+// const sendgrid = require('sendgrid')(process.env.SENDGRID_USER, process.env.SENDGRID_API_KEY);
+
+//for saving images in a folder
+// const fs = require('fs');
+// const path = require('path');
+
+// const directory = 'test';
+
+// fs.readdir(directory, (err, files) => {
+//   if (err) throw err;
+
+//   for (const file of files) {
+//     fs.unlink(path.join(directory, file), err => {
+//       if (err) throw err;
+//     });
+//   }
+// });
 
 //for fetching product details
 router.get('/filterData', async (req, res) => {
@@ -15,6 +35,29 @@ router.get('/filterData', async (req, res) => {
             Error: e.message
         });
     }
+});
+
+router.post('/sendMail', (req, res) => {
+    const msg = {
+        to: req.body.email,
+        from: 'contact.ritikaagarwal@gmail.com',
+        subject: req.body.subject,
+        text: 'Sent from send grid',
+        html: req.body.content
+    };
+    (async () => {
+        try {
+            const response = await sgMail.send(msg);
+            res.send(response);
+        } catch (error) {
+            console.error(error);
+
+            if (error.response) {
+                console.error(error.response.body);
+                res.send(error.response.body);
+            }
+        }
+    })();
 });
 
 module.exports = router;
